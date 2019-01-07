816f8709c507726c9c3fa781eaad91e59a7509da
Connie Hyman <constance.hyman@mchsi.com>
2019-01-07 04:55:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,7 +48,7 @@ class ApplicationController < Sinatra::Base
   delete '/articles/:id' do
     @article = Article.find(params[:id])
     @article.destroy
-    redirect to '/articles'
+    redirect to('/articles')
   end
 
 
