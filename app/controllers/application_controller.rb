fd7588c32167bb7bcff8d8613d10401c43f97d3e
Connie Hyman <constance.hyman@mchsi.com>
2019-01-07 05:29:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,10 +46,14 @@ class ApplicationController < Sinatra::Base
     redirect to("/articles/#{params[:id]}")
   end
 
-  delete '/articles/<%=@article.id%>' do
-  @article = Article.delete(params[:id])
-  redirect to("/articles")
-  # erb :delete
-end
+  delete '/article/:id' do
+    @article_object = Article.delete(params[:id])
+    redirect to("/")
+  end
+#   delete '/articles/<%=@article.id%>' do
+#   @article = Article.delete(params[:id])
+#   redirect to("/articles")
+#   # erb :delete
+# end
 
 end
