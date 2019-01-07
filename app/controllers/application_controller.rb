5389466d04e03bd4a6b6f17d97e0bdbba92b9559
Connie Hyman <constance.hyman@mchsi.com>
2019-01-07 06:05:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,7 +50,7 @@ class ApplicationController < Sinatra::Base
     redirect to("/articles/#{@article.id}")
   end
 
-  delete "/articles/:id" do
+  delete "/articles/<%= @article.id%>" do
     Article.destroy(params[:id])
     redirect to "/articles"
   end
